d08231e9bf3301cfc5c53a9cb1c28ca110da653a
silverwish <jdmcqueen44@gmail.com>
2021-11-24 15:55:17 +0000

    added a problem solving comment above the darker squares block

--- sketch.js
+++ sketch.js
@@ -49,6 +49,20 @@ let blackSquares = function() {
 
 black.addEventListener("click", blackSquares);
 
+// darker squares problem
+
+// - how the code currently works
+ 
+//  when the greyscale button is clicked, variables for r, g and b are set. A loop then runs on the squares which listens for mouseover events. On event occurrence, a background color for the square is set using the rgb variable values defined previously. Additionally, 0.6 is removed from the value of r, g and b. This means that every time a mouseover event occurs, the rgb value (which applies to all squares) steadily decreases, making each square progressively darker on mouseover.
+ 
+//  - how do i want my code to work?
+ 
+//  I want a square's local rgb value to decrease on mouseover, unaffecting other square's rgb values until they themselves are hovered by the mouse.
+ 
+//  - possible solutions
+ 
+//  Tried using e.currentTarget.style.backgroundColor = "..." to change the background color of the individual square on which the event fired, but it seems to be functionally equivalent to the variable 'square'
+
 let darkerSquares = function() {
     let r = 240;
     let g = 240;
