93b7b68c0efed4f7eff1d5cfb5903ad59b247e28
silverwish <jdmcqueen44@gmail.com>
2022-01-12 15:05:42 +0000

    removed some comments and edited others; wrote in the functions which change grid dimensions as the slider value is changed

--- sketch.js
+++ sketch.js
@@ -1,27 +1,41 @@
-let createSquares = () => {
-    let slider = document.getElementById("dimensions");
-    let sliderPara = document.querySelector(".slider-value p");
-    let sliderDefault = slider.getAttribute("value");
-
-    sliderPara.textContent = sliderDefault;
-
-    slider.addEventListener("input", (e) => {
-        let sliderValue = e.currentTarget.value;
-
-        sliderPara.textContent = sliderValue;
-    });
-
+let createSquares = (dimension) => {
     let squareBox = document.querySelector(".squares-cntr");
-    squareBox.style.gridTemplateColumns = "repeat(16, auto)";
+    squareBox.style.gridTemplateColumns = `repeat(${dimension}, auto)`;
     
-    for (let i = 0; i < 256; i++) {
+    for (let i = 0; i < (dimension*dimension); i++) {
         let newSquare = document.createElement("div");
         newSquare.className = "square";
         squareBox.appendChild(newSquare);
     }
 }
 
-createSquares();
+window.addEventListener("load", createSquares(16));
+
+let removeSquares = () => {
+    let squareCntr = document.querySelector(".squares-cntr");
+
+    if (squareCntr.hasChildNodes()) {
+        let allSquares = document.getElementsByClassName("square");
+
+        while (allSquares.length > 0) {
+            squareCntr.removeChild(allSquares[0]);
+        }
+    }
+}
+
+let slider = document.getElementById("dimensions");
+let sliderPara = document.querySelector(".slider-value p");
+let sliderDefault = slider.getAttribute("value");
+
+sliderPara.textContent = sliderDefault;
+
+slider.addEventListener("input", (e) => {
+    let sliderValue = e.currentTarget.value;
+    sliderPara.textContent = sliderValue;
+
+    removeSquares(); // remove the previous grid
+    createSquares(sliderValue);
+});
 
 let square = document.querySelector(".square");
 let squares = document.querySelectorAll(".square");
@@ -68,17 +82,14 @@ let blackSquares = () => {
 blackSquares();
 
 // darker squares problem
-
 // - how the code currently works
  
 // when the greyscale button is clicked, variables for r, g and b are set. A loop then runs on the squares which listens for mouseover events. On event occurrence, a background color for the square is set using the rgb variable values defined previously. Additionally, 0.6 is removed from the value of r, g and b. This means that every time a mouseover event occurs, the rgb value (which applies to all squares) steadily decreases, making each square progressively darker on mouseover.
  
 // - how do i want my code to work?
- 
 // I want a square's local rgb value to decrease on mouseover, unaffecting other square's rgb values until they themselves are hovered by the mouse.
  
 // - possible solutions
- 
 // Tried using e.currentTarget.style.backgroundColor = "..." to change the background color of the individual square on which the event fired, but it seems to be functionally equivalent to the variable 'square'
 
 let greyscaleSquares = () => {
@@ -148,9 +159,6 @@ let clearSquares = () => {
 
 clearSquares();
 
-// create a loop
-// after each mouseover, add 5% more black to the background color
-
 let displayFooter = () => {
     let footer = document.querySelector("footer p");
 
@@ -167,11 +175,3 @@ let displayFooter = () => {
 }
 
 displayFooter();
-
-// you might have to specify the size of the squares in javascript for all dimension options
-
-// create a function which handles the random color hover effect
-// call the function on hover
-
-// If btn is clicked, then mouse hover changes the background colour of the squares
-// mouseover event listener inside a click event listener?
